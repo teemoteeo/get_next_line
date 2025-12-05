9137854270f3f5638b39e0ce8ee2a6ad47fb743d
Timoteo Costantini Scala <timoteo@MacBook-Pro-di-Timoteo.local>
2025-12-05 15:33:11 +0100

    antigravity

--- get_next_line.h
+++ get_next_line.h
@@ -13,15 +13,21 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
 char	*ft_strjoin(char const *s1, char const *s2);
-void	*ft_calloc(size_t count, size_t size);
 char	*ft_strchr(const char *s, int c);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,55 +6,56 @@
 /*   By: tcostant <tcostant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:04:48 by tcostant          #+#    #+#             */
-/*   Updated: 2025/12/04 12:09:32 by tcostant         ###   ########.fr       */
+/*   Updated: 2025/12/05 14:40:00 by antigravity      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdint.h>
 
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t	ft_strlen(const char *str)
 {
-	char	*result;
 	size_t	i;
-	size_t	total_len;
 
-	total_len = ft_strlen(s1) + ft_strlen(s2);
-	result = malloc((total_len + 1) * sizeof(char));
-	if (!result)
-		return (NULL);
+	if (!str)
+		return (0);
 	i = 0;
-	while (*s1)
+	while (str[i] != '\0')
 	{
-		result[i] = *s1;
-		s1++;
 		i++;
 	}
-	while (*s2)
-	{
-		result[i] = *s2;
-		s2++;
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
+	return (i);
 }
 
-void	*ft_calloc(size_t count, size_t size)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	void	*result;
+	char	*str;
+	size_t	i;
+	size_t	j;
 
-	if (size != 0 && count > SIZE_MAX / size)
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
 		return (NULL);
-	result = malloc(count * size);
-	if (!result)
-		return (NULL);
-	ft_bzero(result, (size * count));
-	return (result);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
@@ -80,14 +81,20 @@ void	ft_bzero(void *s, size_t n)
 	}
 }
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
+	size_t	src_len;
 	size_t	i;
 
+	src_len = ft_strlen(src);
+	if (dstsize == 0)
+		return (src_len);
 	i = 0;
-	while (str[i] != '\0')
+	while (src[i] && i < dstsize - 1)
 	{
+		dst[i] = src[i];
 		i++;
 	}
-	return (i);
+	dst[i] = '\0';
+	return (src_len);
 }
